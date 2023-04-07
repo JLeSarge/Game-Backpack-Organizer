674a62f031daf17fbd9e3c4e82a703f46d45e488
Eric-Douglas-Johnson <77891599+Eric-Douglas-Johnson@users.noreply.github.com>
2023-04-06 21:49:15 -0500

    clean up

--- namespace GameBackPackOrganizer.cs
+++ namespace GameBackPackOrganizer.cs
@@ -2,82 +2,77 @@ namespace GameBackPackOrganizer.GameEngine
 {
     public class BackPack
     {
-        public BackPackSlot[5][4] slot{get; set;}
+        private record struct Item(int Id, int value);
+        private record struct Slot(int Id, Item item);
+        
+        public Slot[,] Slots { get; set; }
 
-        public BackPack(item[][] items)
+        public BackPack(Item[,] items)
         {
-            for(int i=0;i<items.Length(0);i++)
+            for(int i = 0; i < items.Length(0); i++)
             {
-                for(int j=0;j<items.Length(1);j++)
+                for(int j = 0; j < items.Length(1); j++)
                 {
                     slot[i][j].slottedItem = items[i][j];
                 }
             }
         }
-    }
 
-    public class BackPackSlot
-    {
-        public int slotID{get; set;}
-        public item slottedItem{get; set;}
-    }
-
-    public class item 
-    {
-        public int itemID{get; set;}
-        public int itemValue{get; set;}
-    }
-
-    public class SortByValue
-    {
-        public static void SortByValue(BackPack input)
+        public BackPack(int xDimension, int yDimension)
         {
-            int i = 0;
+            Slots = new Slot[xDimension][yDimension];
+        }
+
+        public void SortByValue()
+        {
             int digit = 0;
             item[] itemArray = new int[20];
+
             //first take all items and put them into a new array to be sorted more easily
-            for(i=0;i<input.slot.Length(0);i++)
+            for(int i = 0; i < Slots.Length(0); i++)
             {
-               for(j=0;j<input.slot.Length(1);j++)
+               for(j = 0; j < Slots.Length(1); j++)
                 {
-                   itemArray[i] = input.slot[i][j].slottedItem;
+                   itemArray[i] = Slots[i][j].slottedItem;
                    i++;
                 } 
             }
+
             // this is to find the max value for digit, so we can sort by the digits
-            for(i = 0;i<itemArray.Length;i++)
+            for(int i = 0; i < itemArray.Length; i++)
             {
                 int tempDigit = itemArray[i].itemValue%10;
-                if(tempDigit>digit)
+                if(tempDigit > digit)
                 {
                     digit = tempDigit;
                 }
             }
+
             // now its time to actually sort through all digits
-            for(int g=1;g<=digit;g++)
+            for(int g = 1; g <= digit; g++)
             {
-                for(i=1;i<itemArray.Length;i++)
+                for(i = 1; i < itemArray.Length; i++)
                 {
                     int key = itemArray[i]%(10^g);
-                    int j = i-1;
-                    while(j>0 && itemArray[j]%(10^g)<key)
+                    int j = i - 1;
+                    while(j > 0 && itemArray[j] % (10^g) < key)
                     {
                         itemArray[j+1] = itemArray[j];
                         j--;
                     }
-                    itemArray[j+1]=key;
+                    itemArray[j+1] = key;
                 }
             }
+
             //lastly we must put all the sorted items back into the backpack
-            for(i=0;i<input.slot.Length(0);i++)
+            for(int i = 0; i < Slots.Length(0); i++)
             {
-               for(j=0;j<input.slot.Length(1);j++)
+               for(j = 0; j < Slots.Length(1); j++)
                 {
-                   input.slot[i][j].slottedItem = itemArray[i];
+                   Slots[i][j].slottedItem = itemArray[i];
                    i++;
                 } 
             }
-            return(input);
         }
     }
 }
